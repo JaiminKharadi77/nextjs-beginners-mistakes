bce1acb73866738f2e327c49d02ea55122d68bbc
Jaimin1999 <learnwithjaimin@gmail.com>
2025-03-30 19:20:26 +0530

    Mistake #4: Thinking that a server component becomes a client component if you wrap it inside a client component

--- src/app/components/Product.js
+++ src/app/components/Product.js
@@ -1,5 +1,4 @@
-// This is a server component for (reference to my future self :])
-// In nextjs 14 by default all components are server components
+
 export default async function Product() {
   const res = await fetch("https://fakestoreapi.com/products/3");
   const product = await res.json();
--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,23 @@
-//1) "use client"; // This will work but it's not the best way to do it because it's not server side rendered
-//2) It will convert Product to client components ,but Product is a server component
-//3) So it will be to use the "use client" in client components itself
-
 import { Product, FavoriteBtn } from "@/app/components";
-
+import ThemeProvider from "@/app/context/ThemeContext";
 export default function Home() {
   return (
     <main className="flex flex-col items-center mt-32 gap-12 text-xl">
       <h1>My Stores</h1>
-      <Product />
+      <ThemeProvider>
+        {/* This is works we can pass server components as children or props 
+        to client components and it still be server components.
+        IMPORTANT: Server component will only be converted to client component if
+        imported where 'use client' directive is used or its client component itself.
+        */}
+        <Product />
+      </ThemeProvider>
       <FavoriteBtn />
     </main>
   );
 }
+
+// ANOTHER IMPORTANT NOTE:
+// Using state management in server components is not recommended because it's not server side rendered.
+// It's better to use state management in client components.
+// for example redux,context api,zustand,etc.
