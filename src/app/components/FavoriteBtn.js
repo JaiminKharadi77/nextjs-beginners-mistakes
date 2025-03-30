efda7e35889532095a64c5b1fcf021b66e7da3e9
Jaimin1999 <learnwithjaimin@gmail.com>
2025-03-30 19:48:36 +0530

    9: Using browser API's (e.g. localStorage)...
    10: Getting hydration errors

--- src/app/components/FavoriteBtn.js
+++ src/app/components/FavoriteBtn.js
@@ -1,15 +1,26 @@
-"use client"; // 4) This will work because it's a client component and it's best practice to use it in client components 
+"use client"; // 4) This will work because it's a client component and it's best practice to use it in client components
 import { useState } from "react";
 import { HeartFilledIcon, HeartIcon } from "@radix-ui/react-icons";
 
 export default function FavoriteBtn() {
   const [isFavorite, setIsFavorite] = useState(false);
+  let isFavoriteBefore;
+  if (typeof window !== "undefined") {
+    // isFavoriteBefore = localStorage.getItem("isFavorite");
+    isFavoriteBefore = true;
+  }
   return (
     <button
       onClick={() => setIsFavorite(!isFavorite)}
       className="p-2 rounded-md bg-zinc-100"
     >
-      {isFavorite ? <HeartFilledIcon /> : <HeartIcon />}
+      <p suppressHydrationWarning>
+        {/* using suppressHydrationWarning to avoid hydration error 
+        IMPORTANT: Use this only when you are sure the value changing will not cause any effect
+        */}
+        {isFavorite ? <HeartFilledIcon /> : <HeartIcon />}
+        {`You have liked this item ${isFavoriteBefore ? "Yes" : "No"}`}
+      </p>
     </button>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,11 @@
 import { Product, FavoriteBtn } from "@/app/components";
-import ThemeProvider from "@/app/context/ThemeContext";
+
 export default function Home() {
   return (
     <main className="flex flex-col items-center mt-32 gap-12 text-xl">
       <h1>My Stores</h1>
-      <ThemeProvider>
-        {/* This is works we can pass server components as children or props 
-        to client components and it still be server components.
-        IMPORTANT: Server component will only be converted to client component if
-        imported where 'use client' directive is used or its client component itself.
-        */}
-        <Product />
-      </ThemeProvider>
+      <Product />
       <FavoriteBtn />
     </main>
   );
 }
-
-// ANOTHER IMPORTANT NOTE:
-// Using state management in server components is not recommended because it's not server side rendered.
-// It's better to use state management in client components.
-// for example redux,context api,zustand,etc.
